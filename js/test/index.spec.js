7cf7c259b0e409945ef3e99155bc229e00e07ab6
Viktor Szegedi <viktor.szegedi@emarsys.com>
2023-10-13 11:41:26 +0200

    add js setup

--- js/test/index.spec.js
+++ js/test/index.spec.js
@@ -2,7 +2,7 @@ import { expect } from "chai";
 import { gameOfLifeJs } from "../app.js";
 
 describe('gameOfLifeJs', () => {
-  it('should return Hello Dojoers!', () => {
-    expect(gameOfLifeJs()).to.be.equal('Hello Dojoers!')
+  it('should return Hello Dojoers !', () => {
+    expect(gameOfLifeJs()).to.be.equal('Hello Dojoers !')
   });
 });
